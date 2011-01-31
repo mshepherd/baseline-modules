b7934ec8fb9bfe94597e9b28b11cde030a72a263
mshepherd <matthew@illuminateict.org.uk>
2011-01-31 14:46:33 +0000

    node_gallery updated to most recent dev, fixing the empty gallery bug

--- node_gallery/js/ng_plupload.js
+++ node_gallery/js/ng_plupload.js
@@ -1,4 +1,4 @@
-// $Id: ng_plupload.js,v 1.1.2.1 2010/12/22 15:27:51 justintime Exp $
+// $Id: ng_plupload.js,v 1.1.2.2 2010/12/30 10:11:38 scroogie Exp $
 /**
  * @file
  * Node Gallery additions to plupload page.
@@ -10,7 +10,21 @@
 Drupal.behaviors.nodeGalleryPlupload = function (context) {
   var self = this;
   this.context = context;
-  this.redirectTarget = Drupal.settings.node_gallery.redirectTarget;
+  this.gid = Drupal.settings.node_gallery.gid;
+  this.galleryType = Drupal.settings.node_gallery.galleryType;
+  this.errorMessage = '';
+
+  var updateGallery = function (gallery) {
+    if (typeof gallery.is_error != undefined) {
+      self.errorMessage = gallery.message;
+    }
+    self.gid = gallery.nid;
+  }
+
+  var galleryCreationFailed = function () {
+    self.errorMessage = Drupal.t("Failed to create gallery: Request failed.");
+    self.uploader.stop();
+  }
 
   var init = function () {
     // check if upload instance is already there, else try again later.
@@ -21,7 +35,32 @@ Drupal.behaviors.nodeGalleryPlupload = function (context) {
     }
     self.uploader.bind('FileUploaded', function () {
       if (self.uploader.total.queued == 0) {
-        window.location.href = self.redirectTarget;
+        window.location.href = Drupal.settings.basePath + 'node/' + self.gid + '/edit';
+      }
+    });
+    self.uploader.bind('BeforeUpload', function () {
+      if (!self.gid || self.gid < 0) {
+        // create the gallery on the fly.
+        var url = Drupal.settings.basePath + 'node-gallery/json/gallery/create/' + self.galleryType;
+        // we use a synchronous call here, so plupload waits for us.
+        $.ajax({
+          url: url,
+          success: updateGallery,
+          error: galleryCreationFailed,
+          dataType: 'json',
+          async: false
+        });
+        if (!self.gid || self.gid < 0) {
+          if (self.errorMessage.length < 1) {
+            self.errorMessage = Drupal.t("Failed to create gallery. Unknown error occured.");
+          }
+          alert(self.errorMessage);
+          self.uploader.stop();
+          self.uploader.trigger('Error', {code: "102", message: self.errorMessage, id: self.uploader.id});
+        } else {
+          // update plupload query string with correct gallery nid.
+          self.uploader.settings.url = self.uploader.settings.url.replace(/gid=-1/i, "gid=" + self.gid);
+        }
       }
     });
   }
